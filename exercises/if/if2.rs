c9d7d51c6bf4fb0598ac1c1cf13f1779013d83f3
kushal.pokhrel <kushal.pokhrel@sireto.io>
2023-05-05 22:50:42 +0545

    if and modules completed

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -9,9 +9,10 @@
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
-    } else {
-        1
+    } else if fizzish=="fuzz" {
+        "bar"
     }
+    else {"baz"}
 }
 
 // No test changes needed!
